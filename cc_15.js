39010794e5316bb87267f66a243e6e77850f63d9
Julia <lorrayne@usf.edu>
2025-03-16 21:51:18 -0400

    Task 3 - Removing Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -5,7 +5,7 @@ const riskDashboard = document.getElementById("riskDashboard");
 // Print "Risk Dashboard Loaded" to the console.
 console.log("Risk Dashboard Loaded");
 
-//Task 2
+//Task 2 and task 
 function addRiskItem(riskName, riskLevel, department) {
     const riskCard = document.createElement("div");
     riskCard.classList.add("riskCard");
@@ -19,11 +19,16 @@ function addRiskItem(riskName, riskLevel, department) {
     const nameDepartment = document.createElement("p");
     nameDepartment.textContent = `Department: ${department}`;
 
+    const resolveButton = document.createElement("button"); // task 3:added resolve button
+    resolveButton.textContent = "Resolve";
+    resolveButton.addEventListener("click", function () {
+        riskCard.remove();
+    });
+
     riskCard.appendChild(nameRisk);
     riskCard.appendChild(levelRisk);
     riskCard.appendChild(nameDepartment);
+    riskCard.appendChild(resolveButton);
 
     document.getElementById("riskDashboard").appendChild(riskCard);
 };
-
-
