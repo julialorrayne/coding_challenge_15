13326d5891b2f5b9021fd6bd9701e2a20b3aa8c7
Julia <lorrayne@usf.edu>
2025-03-16 21:26:03 -0400

    Task 2 - Adding Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -5,3 +5,25 @@ const riskDashboard = document.getElementById("riskDashboard");
 // Print "Risk Dashboard Loaded" to the console.
 console.log("Risk Dashboard Loaded");
 
+//Task 2
+function addRiskItem(riskName, riskLevel, department) {
+    const riskCard = document.createElement("div");
+    riskCard.classList.add("riskCard");
+
+    const nameRisk = document.createElement("p");
+    nameRisk.textContent = `Risk Name: ${riskName}`;
+
+    const levelRisk = document.createElement("p");
+    levelRisk.textContent = `Risk Level: ${riskLevel}`;
+
+    const nameDepartment = document.createElement("p");
+    nameDepartment.textContent = `Department: ${department}`;
+
+    riskCard.appendChild(nameRisk);
+    riskCard.appendChild(levelRisk);
+    riskCard.appendChild(nameDepartment);
+
+    document.getElementById("riskDashboard").appendChild(riskCard);
+};
+
+
