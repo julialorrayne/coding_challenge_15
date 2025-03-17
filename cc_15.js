3138e11376052dc74a532ed3c716f1bac3e99fd8
Julia <lorrayne@usf.edu>
2025-03-16 20:11:03 -0400

    Task 1 - Base Structure Setup.

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+//Task 1
+//select the riskDashboard container
+const riskDashboard = document.getElementById("riskDashboard");
+
+// Print "Risk Dashboard Loaded" to the console.
+console.log("Risk Dashboard Loaded");
+
